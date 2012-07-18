0d7a7058dcd84a228f297609e1b926af741c80d6
Jon Lynch <jonnylynchy@gmail.com>
2012-07-18 17:40:32 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EventManagementApp::Application.config.secret_token = 'ae5d0e0345efb19ab8ab94b9fc1c6f7b2cad005433043356303fa08af718c76308558f35bc581d8fd8e65b30d089bdd20340f45270cfebb7626b41b30a778fe7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+EventManagementApp::Application.config.session_store :cookie_store, key: '_EventManagementApp_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# EventManagementApp::Application.config.session_store :active_record_store
